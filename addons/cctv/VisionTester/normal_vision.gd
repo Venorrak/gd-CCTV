a42e6b02ce31098032c4aedebed2593e2a326e28
Venorrak <jaudet199@gmail.com>
2026-07-06 16:10:34 -0400

    normal vision default visual range

--- addons/cctv/VisionTester/normal_vision.gd
+++ addons/cctv/VisionTester/normal_vision.gd
@@ -1,9 +1,9 @@
 ## can see if within a certain range and in the frustum
 class_name NormalVision extends VisionTester
-@export var visual_range_limit : float = 9999
+@export var visual_range_limit : float = 100
 
 func can_see_target(target: Node3D, camera: CCTVCamera) -> bool:
-    if camera.distance_to_target() > visual_range_limit: return false
-    if camera.is_target_in_frustum():
-        return true
-    return false
+	if camera.distance_to_target() > visual_range_limit: return false
+	if camera.is_target_in_frustum():
+		return true
+	return false
